9a84e811528363bbf552855ee0b0641478a36cb7
Robert Kohr <spam@robkohr.com>
2022-09-02 07:49:15 -0700

    auto show when reach 5000 scroll point

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* eslint-disable quotes */
 // Type your JavaScript code here.
 const limitedSites = [
+  "quora",
   "amazon",
   "facebook",
   "reddit",
@@ -12,15 +13,28 @@ const limitedSites = [
 let isLimitedSite = false;
 for (let site of limitedSites) {
   // eslint-disable-next-line no-undef
-  if (document.location.host.includes(site)) {
-    isLimitedSite = true;
+  if (document.location.href.includes(site)) {
+    isLimitedSite = site;
+    console.log({ siteMatch: site });
   }
 }
+console.log({ isLimitedSite });
+
+let showCoverRan = false;
 if (isLimitedSite) {
   setTimeout(showCover, 1000);
 }
 
+setInterval(() => {
+  console.log(document.documentElement.scrollTop, showCoverRan);
+  if (!showCoverRan && document.documentElement.scrollTop > 5000) {
+    showCover();
+  }
+}, 2000);
+
 function showCover() {
+  showCoverRan = true;
+
   document.querySelector("#hide-overlay").style.display = "block";
   document.querySelector("#show-page-button").style.display = "none";
   document.querySelector("#show-page-countdown").style.display = "block";
@@ -30,7 +44,7 @@ function showCover() {
       .querySelector(".ytp-play-button")
       .attributes.title.value.includes("Pause")
   ) {
-    document.querySelector(".ytp-play-button").click();
+    //     document.querySelector(".ytp-play-button").click()
   }
   const waitPeriod = 20;
   let startTime = new Date().getTime();
